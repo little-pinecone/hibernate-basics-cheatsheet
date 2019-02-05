bbde3ed6cf7af272fa970a81a8dd95dcd59f034e
little_pinecone <marta.szymek.mail@gmail.com>
2019-02-05 10:37:28 +0100

    Initial commit.

---
+++ src/main/java/in/keepgrowing/hibernatebasicscheatsheet/HibernateBasicsCheatsheetApplication.java
@@ -0,0 +1,14 @@
+package in.keepgrowing.hibernatebasicscheatsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateBasicsCheatsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateBasicsCheatsheetApplication.class, args);
+	}
+
+}
+
